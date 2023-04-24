ae56fcb63c9be60b0813f9bc51cbf169f21a55f0
choihansu97 <jjpp9712@naver.com>
2023-04-24 17:56:21 +0900

    [#7]feature-msw

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-const express = require("express");
-const path = require("path");
+const { createServer } = require('http');
+const { setupServer } = require('msw/node');
+const { handlers } = require('./src/mocks/handlers');
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use("/src/dist", express.static(path.join(__dirname, "src", "dist")));
+app.use('/src/dist', express.static(path.join(__dirname, 'src', 'dist')));
 
-app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "src", "dist", "index.html"));
+app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'src', 'dist', 'index.html'));
 });
 
-app.listen(process.env.PORT || 3000, () => console.log("Server running..."));
+const port = process.env.PORT || 3000;
+const server = createServer(app);
+
+const worker = setupServer(...handlers);
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+
+    worker.listen();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             filename: "index.html",
-            template: path.resolve(__dirname, "src", "public", "index.html"),
+            template: path.resolve(__dirname, "public", "index.html"),
             inject: 'body',
         }),
         new CleanWebpackPlugin({
