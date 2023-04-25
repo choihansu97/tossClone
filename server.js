47bcca9961c25366edef6a8093cb0193e3906c87
choihansu97 <jjpp9712@naver.com>
2023-04-25 18:03:51 +0900

    [#7]feature:msw구현

--- server.js
+++ server.js
@@ -1,24 +1,10 @@
 const { createServer } = require('http');
 const { setupServer } = require('msw/node');
-const { handlers } = require('./src/mocks/handlers');
-const express = require('express');
-const path = require('path');
+const { app } = require('./app');
+const {handlers} = require('./src/handlers');
 
-const app = express();
+const server = createServer(setupServer(...handlers), app);
 
-app.use('/src/dist', express.static(path.join(__dirname, 'src', 'dist')));
-
-app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'src', 'dist', 'index.html'));
-});
-
-const port = process.env.PORT || 3000;
-const server = createServer(app);
-
-const worker = setupServer(...handlers);
-
-server.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-
-    worker.listen();
+server.listen(3000, () => {
+    console.log(`Server listening on http://localhost:3000`)
 });
