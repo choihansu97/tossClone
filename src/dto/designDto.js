924284e5d7e54356d0a5c2b69a61a96dfa26b745
choihansu97 <jjpp9712@naver.com>
2023-04-25 18:15:56 +0900

    [#8]feature:DTO구현

---
+++ src/dto/designDto.js
@@ -0,0 +1,8 @@
+export class DesignDto {
+    constructor(id, thumbnail, content, createDate) {
+        this.id = id;
+        this.thumbnail = thumbnail;
+        this.content = content;
+        this.createDate = createDate;
+    }
+}
