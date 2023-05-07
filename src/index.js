f796a95f4fc535a770c5d1a2820d4b2f53b8631e
choihansu97 <60763027+choihansu97@users.noreply.github.com>
2023-05-07 21:22:50 +0900

    fate(index):header,footer 컴포넌트 등록

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
-import { CreateRouter } from './router.js';
+import { CreateRouter } from "./router.js";
+import "./components/header/header";
+import "./components/footer/footer";
+import tech from './techArticleList.js';
+import design from "./designArticleList.js";
+import articleView from "./articleView.js";
 
 const router = new CreateRouter();
-const container = document.querySelector('#app');
+const app = document.querySelector("#app");
 
-const pages = {
-  home: () => (container.innerText = 'home'),
-  detail: () => (container.innerHTML = 'detail'),
-  posts: ({ id }) => (container.innerHTML = `posts ${id}`),
-  notFoundComponent: () => (container.innerText = '404 Not Found'),
+const header = document.createElement("app-header");
+app.before(header);
+
+const footer = document.createElement("app-footer");
+app.after(footer);
+
+const components = {
+  tech: () => (new tech().render(app)),
+  design: () => (new design().render(app)),
+  article: (id) => (new articleView(id).render(app)),
+  notFoundComponent: () => (app.innerHTML = "404 Not Found")
 };
 
 router
-  .addRoute('/', pages.home)
-  .addRoute('/home', pages.home)
-  .addRoute('/detail', pages.detail)
-  .addRoute('/posts/:id', pages.posts)
-  .setNotFound(pages.notFoundComponent)
+  .addRoute("/", components.tech)
+  .addRoute("/tech", components.tech)
+  .addRoute("/design", components.design)
+  .addRoute("/article/:id", components.article)
+  .setNotFound(components.notFoundComponent)
   .start();
