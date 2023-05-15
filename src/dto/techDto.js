28cef90daf49cadeb2ba817f3c05f1b876cdaed6
choihansu97 <jjpp9712@naver.com>
2023-05-15 11:09:10 +0900

    feat(dto): dto 무결성 작업

--- src/dto/techDto.js
+++ src/dto/techDto.js
@@ -1,16 +1,32 @@
+export class EditorDto {
+    constructor(name, position, imageUrl, content) {
+        this.name = name;
+        this.position = position;
+        this.imageUrl = imageUrl;
+        this.content = content;
+    }
+
+    validate() {
+        if (!this.name || !this.position || !this.imageUrl || !this.content) {
+            throw new Error('Editor 정보가 유효하지 않습니다.');
+        }
+    }
+}
+
 export class TechDto {
-    constructor(id, category, thumbnail, title, content, createDate, editorName, editorPosition, editorImageUrl, editorContent) {
+    constructor(id, category, thumbnail, title, content, createDate, editor) {
         this.id = id;
         this.category = category;
         this.thumbnail = thumbnail;
         this.title = title;
         this.content = content;
         this.createDate = createDate;
-        this.editor = {
-            name: editorName,
-            position: editorPosition,
-            imageUrl: editorImageUrl,
-            content : editorContent
-        };
+        this.editor = editor;
+
+        this.validate();
+    }
+
+    validate() {
+        this.editor.validate();
     }
 }
