b1edb00f4cf870cf1ff02287a539b4c0ad139464
choihansu97 <jjpp9712@naver.com>
2023-05-16 17:52:02 +0900

    feat(404): notFoundPage 추가

---
+++ src/notFoundPage.js
@@ -0,0 +1,26 @@
+import AbstractView from "../abstractView";
+
+export default class extends AbstractView {
+    constructor() {
+        super();
+        document.title = '404: This page could not be found';
+    }
+
+    template() {
+        let articleHtml = `
+            <h1 class="not-found-page__title">404</h1>
+            <div class=".not-found-page__description">This page could not be found</div>
+        `;
+
+        return `
+            <main class="not-found-page">
+              ${articleHtml}
+            </main>`;
+    }
+
+    async render(target) {
+        if (target) {
+            target.innerHTML = `${this.template()}`;
+        }
+    }
+}
