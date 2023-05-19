e73b1bbe74e17e8f2234e9e89fcd2a6262c87fd9
choihansu97 <jjpp9712@naver.com>
2023-05-19 16:58:42 +0900

    refactoring(articleList): 누락파일 techArticleList 등록

--- src/techArticleList.js
+++ src/techArticleList.js
@@ -5,6 +5,8 @@ import { TechDto, EditorDto } from "./dto/techDto";
 const API_BASE_URL = "http://localhost:3000";
 
 export default class extends AbstractView {
+  url = "http://localhost:3000";
+
   constructor() {
     super();
     this.setTitle("토스 기술 블로그, 토스테크 글 목록");
@@ -12,67 +14,60 @@ export default class extends AbstractView {
 
   async setup() {
     const client = new HttpClient({ baseUrl: API_BASE_URL });
+    const response = await client.get({ path: "/api/tech/articles" });
 
-    try {
-      const response = await client.get(
-          { path: "/api/tech/articles" }
-      );
+    const editorData = response.data.map(
+      (data) =>
+        new EditorDto(
+          data.editor.imageUrl,
+          data.editor.name,
+          data.editor.position,
+          data.editor.content
+        )
+    );
 
-      const editorData = response.data.map(
-          (data) =>
-              new EditorDto(
-                  data.editor.imageUrl,
-                  data.editor.name,
-                  data.editor.position,
-                  data.editor.content
-              )
-      );
+    const techArticleList = response.data.map(
+      (article) =>
+        new TechDto(
+          article.id,
+          article.category,
+          article.thumbnail,
+          article.title,
+          article.content,
+          article.createDate,
+          editorData
+        )
+    );
 
-      const techArticleList = response.data.map(
-          (article) =>
-              new TechDto(
-                  article.id,
-                  article.category,
-                  article.thumbnail,
-                  article.title,
-                  article.content,
-                  article.createDate,
-                  editorData
-              )
-      );
-      return techArticleList;
-    } catch (error) {
-      console.error("오류가 발생했습니다:", error);
-      location.href = `${API_BASE_URL}`;
-    }
+    return techArticleList;
   }
 
   template(techArticleList) {
     let articleHtml = "";
     for (const article of techArticleList) {
       articleHtml += `
-          <li class="article-list-item">
-            <a class='article-list-item-target' href="/tech/article/${article.id}">
-              <img class='article-image' src="${article.thumbnail}" alt="${article.title}">
-              <div class="article-list-item-detail">
-                <h2 class="article-item-title">${article.title}</h2>
-                <p class="article-item-detail">${article.content}</p>
-                <p class="article-item-date">${article.createDate}</p>
-              </div>
-            </a>
-          </li>
-        `;
+        <li class="article-list__item">
+          <a class="article-list__link" href="/tech/article/${article.id}">
+            <img class="article-list__thumbnail" src="${article.thumbnail}" alt="${article.title}">
+            <div class="article-list-content">
+              <h2 class="article-list-content__title">${article.title}</h2>
+              <p class="article-list-content__text">${article.content}</p>
+              <p class="article-list-content__date">${article.createDate}</p>
+            </div>
+          </a>
+        </li>
+    `;
     }
 
     return `
-            <main class="main-article-container">
-              <div class="main-article-wrapper">
-                <h1 class="main-article-header-title">개발</h1>
-                <ul class="main-article-list">
-                  ${articleHtml}
-                </ul>
-              </div>
-            </main>
+      <article class="article-list-container">
+        <div class="article-list-inner">
+          <h1 class="article-list-inner__title">개발</h1>
+          <ul class="article-list">
+            ${articleHtml}
+          </ul>
+         </div>
+        </article>
       `;
   }
 
