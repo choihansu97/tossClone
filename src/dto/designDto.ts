ea316b102a450b26b80a60fb9146b650a06e7492
choihansu97 <60763027+choihansu97@users.noreply.github.com>
2023-05-29 13:47:45 +0900

    feat(dto): dto ts 마이그레이션

---
+++ src/dto/designDto.ts
@@ -0,0 +1,53 @@
+export class EditorDto {
+    imageUrl: string;
+    editorName: string;
+    position: string;
+    content: string;
+
+    constructor(imageUrl: string, editorName: string, position: string, content: string) {
+        this.imageUrl = imageUrl;
+        this.editorName = editorName;
+        this.position = position;
+        this.content = content;
+    }
+
+    validate(): void {
+        if (!this.editorName || !this.position || !this.imageUrl || !this.content) {
+            throw new Error("Editor 정보가 유효하지 않습니다.");
+        }
+    }
+}
+
+export class DesignDto {
+    id: string;
+    category: string;
+    thumbnail: string;
+    title: string;
+    content: string;
+    createDate: string;
+    editor: EditorDto[];
+
+    constructor(
+        id: string,
+        category: string,
+        thumbnail: string,
+        title: string,
+        content: string,
+        createDate: string,
+        editor: EditorDto[]
+    ) {
+        this.id = id;
+        this.category = category;
+        this.thumbnail = thumbnail;
+        this.title = title;
+        this.content = content;
+        this.createDate = createDate;
+        this.editor = editor;
+
+        this.validate();
+    }
+
+    validate(): void {
+        this.editor.forEach((editor) => editor.validate());
+    }
+}
