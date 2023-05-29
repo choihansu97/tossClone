23f5cf365f5c2a63e392e80402473956e6dece88
choihansu97 <60763027+choihansu97@users.noreply.github.com>
2023-05-29 14:04:20 +0900

    feat(index): 누락된 파일 및 ts 마이그레이션

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const path = require("path");
